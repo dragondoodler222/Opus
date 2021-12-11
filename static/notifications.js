d796a38639acc24dadafffadb8d9d97cc0105136
GeryonDev <64339881+GeryonDev@users.noreply.github.com>
2021-12-11 15:13:49 -0500

    ee

--- static/notifications.js
+++ static/notifications.js
@@ -1,4 +1,5 @@
 function postData(type, id, username, index) {
 	console.log("type");
 	$.post("",{request_type: (type == 1 ? "Accept" : "Reject"), identifier: id, user: username, i: index},() => window.location.reload());
+	window.location.reload();
 }
